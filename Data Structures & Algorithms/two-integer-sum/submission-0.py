63b6e3f396eee12430fe2d18328a1873468b7cd3
Yamiko <120058235+yvmiko@users.noreply.github.com>
2026-09-21 17:02:07 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        seen: Dict[int, int] = {}
+
+        for i, num in enumerate(nums):
+            complement = target - nums[i]
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[nums[i]] = i
+
+        return []
